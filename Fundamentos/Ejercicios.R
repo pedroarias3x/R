c59e595c82c6046099f7c35d72148fc4d75396a9
Pedro Arias <pedroarias3x@gmail.com>
2023-11-26 09:34:07 +0100

    Ejercicios temario fundamentos

---
+++ Fundamentos/Ejercicios.R
@@ -0,0 +1,33 @@
+#Ejercicio 1
+numero <- 10
+nombre <- "Pedro"
+#Ejercicio 2
+class(numero)
+is.numeric(numero)
+#Ejercicio 3
+numero = numero + numero * 2
+#Ejercicio 4
+edades = c(30,34,4)
+informacion = list(nombre = "Pedro", edad = 30)
+#Ejercicio 5
+is.character(nombre)
+es_numerico = TRUE
+is.logical(es_numerico)
+#Ejercicio 6
+mayor_de_edad <- edades[1] >= 18
+mayor_de_edad
+#Ejercicio 7
+esta_presente <- 30 %in% edades
+esta_presente
+#Ejercicio 8
+es_mayor <- 2*numero >= edades[3]
+es_mayor
+#Ejercicio 9
+condicion1 <- TRUE
+condicion2 <- TRUE
+condiciones_verdaderas <- condicion1 & condicion2
+condiciones_verdaderas
+#Ejercicio 10
+verdadero <- TRUE
+no_verdadero  <- !verdadero
+no_verdadero
