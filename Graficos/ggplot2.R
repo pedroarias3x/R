04aa3b6e573bf1ef73c600e8104dab9297c2f62e
Pedro Arias <pedroarias3x@gmail.com>
2023-12-06 09:00:51 +0100

    ggplot2

---
+++ Graficos/ggplot2.R
@@ -0,0 +1,24 @@
+#Instalacion de las librerias
+install.packages("ggplot2")
+install.packages("gapminder")
+install.packages("dplryr")
+library(ggplot2)
+library(gapminder)
+library(dplyr)
+
+
+head(gapminder,n=10)
+
+#Filtramos por pais España
+spain <- filter(gapminder, country == "Spain")
+head(spain)
+
+
+ggplot(data = spain,
+       aes(x=year, y=lifeExp)) + 
+        geom_line(color = "red") + 
+        geom_point(size = 3, color = "green")+
+        geom_text(aes(label = lifeExp))+
+        labs(title = "Evolucion esperanza de vida en España",
+             x = "Años",
+             y = "Esperanza de vida")
